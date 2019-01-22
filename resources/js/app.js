14339adb8e05fb637e5a450f73709e106e23be7f
Usman <mohammadusman666@gmail.com>
2019-01-22 14:21:22 +0500

    video10

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,6 +16,7 @@ Vue.use(VueRouter)
 // array of routes
 let routes = [
     { path: '/dashboard', component: require('./components/Dashboard.vue').default },
+    { path: '/users', component: require('./components/Users.vue').default },
     { path: '/profile', component: require('./components/Profile.vue').default }
 ]
 
